5c499189393edd5359e84b0451982269c5d30e0a
AlejandraKeber <alejandrakeber@gmail.com>
2023-03-07 16:45:08 -0500

    Add Nameable to Person

--- person.rb
+++ person.rb
@@ -1,5 +1,8 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -20,6 +23,10 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
 
 person1 = Person.new(12, name: 'Andre', parent_permission: false)
@@ -27,3 +34,4 @@ puts person1.name
 puts person1.age
 puts person1.id
 puts person1.can_use_services?
+puts person1.correct_name
