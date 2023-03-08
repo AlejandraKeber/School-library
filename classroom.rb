a63d7b4a5fa9d8eb4c4f33bb0602d07defa48bf4
AlejandraKeber <alejandrakeber@gmail.com>
2023-03-08 16:55:08 -0500

    Add relationship between Student and Classroom

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,13 @@
 class Classroom
   def initialize(label)
     @label = label
+    @students = []
   end
   attr_accessor :label
+  attr_reader :students
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
 end
